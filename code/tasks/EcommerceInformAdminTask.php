7236854f6346f7fe235574816d524f364aa59a4a
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2011-05-13 05:07:15 +0000

    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_statistics/trunk@1215 950b781c-c544-efab-2f99-90083ee69683

---
+++ code/tasks/EcommerceInformAdminTask.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This class sends a daily email to the shop administrator to inform her / him about sales, abandonned carts, etc..
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ **/
+
+class EcommerceInformAdminTask extends DailyTask {
+
+}
